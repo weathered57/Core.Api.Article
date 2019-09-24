2738b5dbf346c174b6c8d4e90d9ccdbd87c2705d
weathered57 <a_canozdogru57@hotmail.com>
2019-09-25 00:16:28 +0300

    Revert "Revert "Servis katmanın eklenmesi ve makalelerin listelenmesi""
    This reverts commit 7167e3353e44e0f08b24aac42f93a657d3852428.

---
+++ Core.Api_Service/Controllers/ArticleController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Api.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Api_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleController : Controller
+    {
+        IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var articles= _articleService.GetAll();
+            return Ok(articles);
+        }
+       
+    }
+}
