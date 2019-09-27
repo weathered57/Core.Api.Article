22218a5a3a999697ac24259eede0b916093b084b
weathered57 <a_canozdogru57@hotmail.com>
2019-09-27 22:43:55 +0300

    commit

---
+++ Core.Api.Entities/ContextTypes/ArticleModelDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Api.Entities.ContextTypes
+{
+    public class ArticleModelDTO
+    {
+        public string ArticleName { get; set; }
+        public string ArticleTypeName { get; set; }
+        public string ArticleSummary { get; set; }
+    }
+}
