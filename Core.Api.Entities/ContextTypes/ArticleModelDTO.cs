fbcbb9ca4bb3828361494f3c6665acc1c33a7b92
weathered57 <a_canozdogru57@hotmail.com>
2019-09-28 12:14:30 +0300

    ArticleModelDTO revize

--- Core.Api.Entities/ContextTypes/ArticleModelDTO.cs
+++ Core.Api.Entities/ContextTypes/ArticleModelDTO.cs
@@ -6,6 +6,7 @@ namespace Core.Api.Entities.ContextTypes
 {
     public class ArticleModelDTO
     {
+        public int ArticleId { get; set; }
         public string ArticleName { get; set; }
         public string ArticleTypeName { get; set; }
         public string ArticleSummary { get; set; }
